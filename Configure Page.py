bcc3c1840a1f2a9e4bf427ffa7ad968c6aee021a
sunwoonam <nswhelen@gmail.com>
2023-08-24 14:41:36 +1000

    configure page update button

--- Configure Page.py
+++ Configure Page.py
@@ -4,7 +4,7 @@ import sys
 pygame.init()
 
 # Constants
-SCREEN_WIDTH = 1000
+SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 700
 WHITE = (255, 255, 255)
 FONT = pygame.font.Font(None, 36)
@@ -12,47 +12,78 @@ FONT = pygame.font.Font(None, 36)
 
 class ConfigurePage:
     def __init__(self):
-        self.selected_size = "Small"
+        self.selected_size = "10*20"
         self.game_lines = 0
-        self.game_level = 5
+        self.selected_level = "Easy"
         self.game_mode = "Normal"
         self.player_mode = "Player"
 
-        self.size_options = ["Small", "Medium", "Large"]
-        self.size_dropdown_rect = pygame.Rect(50, 150, 300, 50)
-        self.normal_mode_rect = pygame.Rect(50, 450, 200, 50)
-        self.extended_mode_rect = pygame.Rect(300, 450, 200, 50)
-        self.player_mode_rect = pygame.Rect(50, 600, 200, 50)
-        self.ai_mode_rect = pygame.Rect(300, 600, 200, 50)
-        self.close_button_rect = pygame.Rect(800, 650, 150, 30)  # Close button
+        self.size_options = ["10x20", "10x24"]
+        self.size_10x20_rect = pygame.Rect(50, 150, 200, 50)
+        self.size_10x24_rect = pygame.Rect(300, 150, 200, 50)
+        self.easy_mode_rect = pygame.Rect(50, 265, 200, 50)
+        self.medium_mode_rect = pygame.Rect(300, 265, 200, 50)
+        self.hard_mode_rect = pygame.Rect(550, 265, 200, 50)
+        self.normal_mode_rect = pygame.Rect(50, 380, 200, 50)
+        self.extended_mode_rect = pygame.Rect(300, 380, 200, 50)
+        self.player_mode_rect = pygame.Rect(50, 520, 200, 50)
+        self.ai_mode_rect = pygame.Rect(300, 520, 200, 50)
+        self.close_button_rect = pygame.Rect(750, 650, 100, 30)
 
     def draw_configure_page(self, screen):
         screen.fill(WHITE)
 
-        text = FONT.render("Size of the field:", True, pygame.Color('black'))
-        screen.blit(text, (50, 100))
+        # Draw size buttons
+        text = FONT.render("Field size:", True, pygame.Color('black'))
+        screen.blit(text, (50, 110))
+        size_10x20_text = FONT.render("10x20", True, pygame.Color('black'))
+        size_10x24_text = FONT.render("10x24", True, pygame.Color('black'))
+        pygame.draw.rect(screen, pygame.Color('gray'), self.size_10x20_rect)
+        pygame.draw.rect(screen, pygame.Color('gray'), self.size_10x24_rect)
+        pygame.draw.rect(screen, pygame.Color('black'), self.size_10x20_rect, 2)
+        pygame.draw.rect(screen, pygame.Color('black'), self.size_10x24_rect, 2)
 
-        pygame.draw.rect(screen, pygame.Color('gray'), self.size_dropdown_rect)
-        pygame.draw.rect(screen, pygame.Color('black'), self.size_dropdown_rect, 2)
+        screen.blit(size_10x20_text, (self.size_10x20_rect.x + 20, self.size_10x20_rect.y + 15))
+        screen.blit(size_10x24_text, (self.size_10x24_rect.x + 20, self.size_10x24_rect.y + 15))
 
-        size_text = FONT.render(self.selected_size, True, pygame.Color('black'))
-        screen.blit(size_text, (self.size_dropdown_rect.x + 10, self.size_dropdown_rect.y + 10))
-
+        # Draw level buttons
         text = FONT.render("Game level:", True, pygame.Color('black'))
-        screen.blit(text, (50, 400))
+        screen.blit(text, (50, 230))
 
-        # Draw the game level bar
-        level_bar_width = 400
-        level_bar_fill_width = int((self.game_lines - (self.game_level - 5) * 10) / 10 * (level_bar_width / 10))
-        level_bar_rect = pygame.Rect(50, 450, level_bar_width, 20)
-        level_bar_fill_rect = pygame.Rect(50, 450, level_bar_fill_width, 20)
+        pygame.draw.rect(screen, pygame.Color('gray'), self.easy_mode_rect)
+        pygame.draw.rect(screen, pygame.Color('gray'), self.medium_mode_rect)
+        pygame.draw.rect(screen, pygame.Color('gray'), self.hard_mode_rect)
+        pygame.draw.rect(screen, pygame.Color('black'), self.easy_mode_rect, 2)
+        pygame.draw.rect(screen, pygame.Color('black'), self.medium_mode_rect, 2)
+        pygame.draw.rect(screen, pygame.Color('black'), self.hard_mode_rect, 2)
 
-        pygame.draw.rect(screen, pygame.Color('gray'), level_bar_rect)
-        pygame.draw.rect(screen, pygame.Color('black'), level_bar_rect, 2)
-        pygame.draw.rect(screen, pygame.Color('blue'), level_bar_fill_rect)
+        easy_text = FONT.render("Easy", True, pygame.Color('black'))
+        normal_text = FONT.render("Medium", True, pygame.Color('black'))
+        hard_text = FONT.render("Hard", True, pygame.Color('black'))
 
+        screen.blit(easy_text, (self.easy_mode_rect.x + 10, self.easy_mode_rect.y + 10))
+        screen.blit(normal_text, (self.medium_mode_rect.x + 10, self.medium_mode_rect.y + 10))
+        screen.blit(hard_text, (self.hard_mode_rect.x +10, self.hard_mode_rect.y + 10))
+
+        #game mode (normal/extended)
+        text = FONT.render("Game mode:", True, pygame.Color('black'))
+        screen.blit(text, (50, 340))
+
+        pygame.draw.rect(screen, pygame.Color('gray'), self.normal_mode_rect)
+        pygame.draw.rect(screen, pygame.Color('gray'), self.extended_mode_rect)
+
+        pygame.draw.rect(screen, pygame.Color('black'), self.normal_mode_rect, 2)
+        pygame.draw.rect(screen, pygame.Color('black'), self.extended_mode_rect, 2)
+
+        normal_text = FONT.render("Normal", True, pygame.Color('black'))
+        extended_text = FONT.render("Extended", True, pygame.Color('black'))
+
+        screen.blit(normal_text, (self.normal_mode_rect.x + 10, self.normal_mode_rect.y + 10))
+        screen.blit(extended_text, (self.extended_mode_rect.x + 10, self.extended_mode_rect.y + 10))
+
+        #player mode (Player/AI)
         text = FONT.render("Player mode:", True, pygame.Color('black'))
-        screen.blit(text, (50, 550))
+        screen.blit(text, (50, 470))
 
         pygame.draw.rect(screen, pygame.Color('gray'), self.player_mode_rect)
         pygame.draw.rect(screen, pygame.Color('gray'), self.ai_mode_rect)
@@ -67,15 +98,22 @@ class ConfigurePage:
         screen.blit(ai_text, (self.ai_mode_rect.x + 10, self.ai_mode_rect.y + 10))
 
         # Draw close button
-        pygame.draw.rect(screen, pygame.Color('gray'), self.close_button_rect)
+        pygame.draw.rect(screen, pygame.Color('black'), self.close_button_rect)
         pygame.draw.rect(screen, pygame.Color('black'), self.close_button_rect, 2)
-        close_text = FONT.render("Close", True, pygame.Color('black'))
+        close_text = FONT.render("Close", True, pygame.Color('white'))
         screen.blit(close_text, (self.close_button_rect.x + 10, self.close_button_rect.y + 5))
 
     def handle_mouse_click(self, mouse_pos):
-        if self.size_dropdown_rect.collidepoint(mouse_pos):
-            selected_size_index = (self.size_options.index(self.selected_size) + 1) % len(self.size_options)
-            self.selected_size = self.size_options[selected_size_index]
+        if self.size_10x20_rect.collidepoint(mouse_pos):
+            self.selected_size = "10x20"
+        elif self.size_10x24_rect.collidepoint(mouse_pos):
+            self.selected_size = "10x24"
+        elif self.easy_mode_rect.collidepoint(mouse_pos):
+            self.selected_level = "Easy"
+        elif self.medium_mode_rect.collidepoint(mouse_pos):
+            self.selected_level = "Medium"
+        elif self.hard_mode_rect.collidepoint(mouse_pos):
+            self.selected_level = "Hard"
         elif self.normal_mode_rect.collidepoint(mouse_pos):
             self.game_mode = "Normal"
         elif self.extended_mode_rect.collidepoint(mouse_pos):
@@ -90,9 +128,10 @@ class ConfigurePage:
 
 
 def main():
-    # Create the startup screen
+    pygame.mixer.music.load("background.wav")
+    pygame.mixer.music.play(-1)
     startup_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Tetris Startup")
+    pygame.display.set_caption("Tetris Setting")
     configure_page = ConfigurePage()
     clock = pygame.time.Clock()
 
@@ -112,7 +151,6 @@ def main():
 
     pygame.quit()
     sys.exit()
-
 
 if __name__ == "__main__":
     main()
