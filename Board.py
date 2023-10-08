390365eaf5ce62832744f63c01ce6920493871ba
emilygaelic <140472965+emilygaelic@users.noreply.github.com>
2023-10-08 19:11:53 +1000

    Update Board.py

--- Board.py
+++ Board.py
@@ -10,14 +10,14 @@ class GameBoard:
         self.grid = [[0 for j in range(self.cols)] for i in range(self.rows)]
 
 
-    def print_board(self):
+    def PrintBoard(self):
         # prints game board
         for i in range(self.rows):
             for j in range(self.cols):
                 print(self.grid[i][j], end=" ")
             print("\n")
 
-    def colours(self):
+    def Colours(self):
         # colour configurations
         dark_grey = (25, 30, 40)
         light_blue = (21, 204, 209)
@@ -29,29 +29,30 @@ class GameBoard:
         red = (232, 18, 18)
         return [dark_grey, light_blue, blue, orange, purple, yellow, green, red]
 
-    def draw_board(self, game_page):
+    def DrawBoard(self, gamePage):
         # draws the board on the screen  
         for i in range(self.rows): # for the size of the board
             for j in range(self.cols):
-                cell_colour = self.grid[i][j] # check number on grid for which colour to draw
-                cell_colours = self.colours() # get colour 
+                cellColour = self.grid[i][j] # check number on grid for which colour to draw
+                cellColours = self.Colours() # get colour 
                 # draw cell in colour assigned by grid number
-                pygame.draw.rect(game_page, cell_colours[cell_colour], (
+                pygame.draw.rect(gamePage, cellColours[cellColour], (
                     j * self.cell_size + 50, i * self.cell_size + 100, self.cell_size - 1, self.cell_size - 1))
                 # 50 and 100 is the board offset, cell size reduced by 1 to produce grid lines
 
-    def place_block(self, block_cells): # place the new block on grid
-        block_position = [] # identify block's position on board
+
+    def PlaceBlock(self, blockCells): # place the new block on grid
+        blockPosition = [] # identify block's position on board
         center = self.cols // 2 - 2 # subtract 2 for size of block
         for i in range(self.rows): # use block co-ords to find grid location
             for j in range(self.cols):
-                for block_cell in block_cells:
-                    if i == block_cell[0] and j == block_cell[1]:
-                        block_position.append([i, j + center])
-        return block_position
+                for blockCell in blockCells:
+                    if i == blockCell[0] and j == blockCell[1]:
+                        blockPosition.append([i, j + center])
+        return blockPosition
 
 
-    def inside_board(self, cells): 
+    def InsideBoard(self, cells): 
         for cell in cells: 
             # check if block in on grid
             if (cell[0] >= self.rows) or (cell[1] >= self.cols or cell[1] < 0):
@@ -60,19 +61,15 @@ class GameBoard:
             # block has collaided with locked blocked
             x = cell[0]
             y = cell[1]
-            print(self.grid[x][y])
-
             if self.grid[x][y] != 0:
-                print("collision")
                 return False
-
         return True # block is within grid
 
 
-    def lock_block(self, cells, block_num): # lock block in grid
+    def LockBlock(self, cells, blockID): # lock block in grid
         # assign block position with block number on board
         for i in range(len(cells)): 
             x = cells[i][0]
             y = cells[i][1]
-            self.grid[x][y] = block_num
+            self.grid[x][y] = blockID
         
