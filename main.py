5e0810d7bd39c771f4fe1634be4b22ecd93048ef
emilygaelic <140472965+emilygaelic@users.noreply.github.com>
2023-10-15 23:31:17 +1000

    Update main.py

--- main.py
+++ main.py
@@ -6,28 +6,22 @@ from StartupPage import StartupPage
 from GamePage import PlayGame
 
 def main():
-    # start page code
     pygame.init()
-    win = pygame.display.set_mode((1000, 700))
+    screen = pygame.display.set_mode((1000, 700))
     pygame.mouse.set_visible(1)
-    win.fill((255, 255, 255))
+    screen.fill((255, 255, 255))
     startupPage = StartupPage()
-    startupPage.DrawStartupPage(win)
-    go = True
+    startupPage.DrawStartupPage(screen)
     # start page
-    while go:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse = pygame.mouse.get_pos()
-                startupPage.HandleMouseClick(win, mouse)
-
+                startupPage.HandleMouseClick(screen, mouse)
         pygame.display.flip()
-    pygame.quit()
-    sys.exit()
-
 
 if __name__ == "__main__":
     main()
